ad4bd8a27e65597998f7e100df42670b8bce614a
Youssef <bensassi.youssef@gmail.com>
2019-05-29 11:52:42 +0100

    rrr

--- main.cpp
+++ main.cpp
@@ -4,10 +4,7 @@
 
 int main(int argc, char** argv) {
 	std::cout<<"Hello I am youssef......."<<std::endl;
-<<<<<<< HEAD
-=======
 	std::cout<<"Hello All...."<<std::endl;
->>>>>>> copy1
 	std::cout<<"Hello......."<<std::endl;
 	return 0;
 }
