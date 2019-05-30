5ca97e834e79ea60fde68a2ec0c4a071cfb7451e
ybensassi <50930838+ybensassi@users.noreply.github.com>
2019-05-30 15:37:41 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
-	std::cout<<"Hello I am youssef......."<<std::endl;
+	std::cout<<"Hello I am youssef.."<<std::endl;
 	std::cout<<"Hello All......"<<std::endl;
 	std::cout<<"Hello.........."<<std::endl;
 	std::cout<<"End............"<<std::endl;
